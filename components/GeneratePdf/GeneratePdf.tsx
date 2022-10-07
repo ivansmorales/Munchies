e1cb909464b24ce0027bd7827c716fca7464b85e
ivansmorales <ivann_mg@hotmail.com>
2022-10-07 10:28:22 -0500

    Add conditional rendering pdf

--- components/GeneratePdf/GeneratePdf.tsx
+++ components/GeneratePdf/GeneratePdf.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Document, Image, Page, Text, View } from "@react-pdf/renderer";
 import { styles } from "./GeneratePdfStyles";
 
 export const GeneratePdf = ({ data }: any) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(false);
+    setOpen(true);
+    return () => setOpen(false);
+  });
+
   return (
-    <Document>
-      <Page wrap={false} size="A4" style={styles.page}>
-        <View style={styles.section}>
-          <Image src="/template.png" style={styles.pageBackground} />
-        </View>
-        <View style={styles.section1}>
-          <View style={styles.flexRow}>
-            <Text style={styles.header}>NÚMERO DE PEDIDO:</Text>
-            <Text>{data.name}</Text>
-          </View>
-        </View>
-      </Page>
-    </Document>
+    <>
+      {open && (
+        <Document>
+          <Page wrap={false} size="A4" style={styles.page}>
+            <View style={styles.section}>
+              <Image src="/template.png" style={styles.pageBackground} />
+            </View>
+            <View style={styles.section1}>
+              <View style={styles.flexRow}>
+                <Text style={styles.header}>NÚMERO DE PEDIDO:</Text>
+                <Text>{data.name}</Text>
+              </View>
+            </View>
+          </Page>
+        </Document>
+      )}
+    </>
   );
 };
